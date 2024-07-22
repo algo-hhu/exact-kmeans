b5b313f55f692842f47d2309dffec548461ed7f8
Giulia Baldini <giulia.baldini.11@gmail.com>
2024-07-22 18:02:09 +0200

    Add logging and assert to tests

--- tests/test_ilp.py
+++ tests/test_ilp.py
@@ -1,9 +1,21 @@
 import logging
+import sys
 import unittest
 
 import pandas as pd
 
 from exact_kmeans import ExactKMeans
+
+formatter = logging.Formatter(
+    "[%(asctime)s: %(levelname)s/%(filename)s:%(lineno)d] %(message)s"
+)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(logging.DEBUG)
+stdout_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.addHandler(stdout_handler)
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -11,10 +23,14 @@ logging.getLogger().setLevel(logging.INFO)
 class TestILP(unittest.TestCase):
     def test_circles(self) -> None:
         X = pd.read_csv("tests/datasets/circlesuneven_4_20_0.csv")
-        ilp = ExactKMeans(X=X.values, k=4)
+        ilp = ExactKMeans(X=X, k=4)
         res = ilp.optimize()
 
-        print(res)
+        print("Found objective value:", res["objective"])
+
+        assert (
+            res["objective"] == 1276.8469883712723
+        ), f"The found objective value is wrong: {res['objective']}"
 
 
 if __name__ == "__main__":
